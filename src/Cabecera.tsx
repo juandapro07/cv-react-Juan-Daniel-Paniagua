ce2039592a1ee17bda53d730f1a33ba496adb780
juan-daniel-paniagua-tapias <juandanielpt07@gmail.com>
2025-11-10 08:07:50 -0500

    creacion de cabecera para importarlo  en la App

---
+++ src/Cabecera.tsx
@@ -0,0 +1,17 @@
+import './Cabecera.css'
+
+function Cabecera() {
+  return (
+    <section className="info">
+      <h2>Datos Personales</h2>
+      <ul>
+        <li><strong>Edad:</strong> 18 años</li>
+        <li><strong>Ubicación:</strong> Medellín, Colombia</li>
+        <li><strong>Correo:</strong> juandanielpt07@gmail.com</li>
+        <li><strong>Teléfono:</strong> +57 300 238 4567</li>
+      </ul>
+    </section>
+  )
+}
+
+export default Cabecera
